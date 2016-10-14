02d73bf4ffa4153cf98c9cb834f559e81979ad27
sephirot47 <victoriodesica777@hotmail.com>
2016-10-14 18:10:06 +0200

    Collisions with ceil fixed

--- TileMap.cpp
+++ TileMap.cpp
@@ -164,9 +164,9 @@ bool TileMap::collisionMoveUp(const glm::ivec2 &pos, const glm::ivec2 &size, int
 	{
 		if (map[y*mapSize.x + x] != 0)
 		{
-			if (pos.y - tileSize * y - size.y <= 4)
+            if (pos.y <= tileSize * y + tileSize)
 			{
-				*posY = tileSize * (y-1) + size.y;
+                *posY = tileSize * y + tileSize;
 				return true;
 			}
 		}
